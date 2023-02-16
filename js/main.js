8bf9b0df8869897032ed222e5c1921875d93bc9e
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-02-16 18:24:33 +0600

    all added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,43 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const rent = getValue("rent");
   const clothe = getValue("clothe");
   const add = sum(food, rent, clothe);
+  //   validation
+  if (income <= 0) {
+    const incomeP = document.getElementById("income-p");
+    incomeP.innerText = "* Please Enter the Money";
+    return;
+  } else {
+    const incomeP = document.getElementById("income-p");
+    incomeP.innerText = "";
+  }
+  if (food <= 0) {
+    const incomeP = document.getElementById("food-p");
+    incomeP.innerText = "* Please Enter the Money";
+    return;
+  } else {
+    const incomeP = document.getElementById("food-p");
+    incomeP.innerText = "";
+  }
+  if (rent <= 0) {
+    const incomeP = document.getElementById("rent-p");
+    incomeP.innerText = "* Please Enter the Money";
+    return;
+  } else {
+    const incomeP = document.getElementById("rent-p");
+    incomeP.innerText = "";
+  }
+  if (clothe <= 0) {
+    const incomeP = document.getElementById("clothe-p");
+    incomeP.innerText = "* Please Enter the Money";
+    return;
+  } else {
+    const incomeP = document.getElementById("clothe-p");
+    incomeP.innerText = "";
+  }
+  if (income < add) {
+    alert("Not Enough Money");
+    return;
+  }
   textAdd("sum-balance", add);
   const sumBalance = getText("sum-balance");
   const balance = parseInt(income) - parseInt(sumBalance);
@@ -14,9 +51,23 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
 document.getElementById("save-btn").addEventListener("click", function () {
   const income = getValue("income");
   const save = getValue("save");
+  //   validation
+  if (save <= 0) {
+    const incomeP = document.getElementById("save-p");
+    incomeP.innerText = "* Please Enter the Money";
+    return;
+  } else {
+    const incomeP = document.getElementById("save-p");
+    incomeP.innerText = "";
+  }
   const saveTotal = (parseInt(income) / 100) * parseInt(save);
-  textAdd("save-amount", saveTotal);
+
   const totalBalance = getText("total-balance");
+  if (saveTotal > totalBalance) {
+    alert("Not Enough Money");
+    return;
+  }
+  textAdd("save-amount", saveTotal);
   const remainingAmountTotal = parseInt(totalBalance) - parseFloat(saveTotal);
 
   textAdd("remaining-amount", remainingAmountTotal);
